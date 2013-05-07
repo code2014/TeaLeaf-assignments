6f989299cdd9745eff7919b269c547cdc284bda9
suhanma1 <suhanma2012@u.northwestern.edu>
2013-05-07 07:45:32 -0500

    chapter 2

---
+++ firstAssignment/learn to program/2/calc.rb
@@ -0,0 +1,8 @@
+puts 1+2
+print 'How many hours are in a year?'
+puts 365 * 24
+print 'How many minutes are in a decade?'
+puts 10*365*24*60
+
+print 'how old am I?'
+puts (1025*1000000).to_f/(365*24*60*60).to_f
